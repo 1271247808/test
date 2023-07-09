79f2864511d8a3cd5d96f02955f9e98219ed08fc
广肀口 <1271247808@qq.com>
2023-07-09 16:43:56 +0800

    清零！！静待题一完美版（除了蜂鸣器 led）

--- Hardware/Buzzer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BUZZER_H
-#define __BUZZER_H
-
-void Buzzer_Init(void);
-void Buzzer_On(void);
-void Buzzer_Off(void);
-void Buzz_300ms(void);
-
-#endif
--- System/NVIC.h
+++
@@ -1,6 +0,0 @@
-#ifndef __NVIC_H
-#define __NVIC_H
-
-void NVIC_Config(void);
-
-#endif
